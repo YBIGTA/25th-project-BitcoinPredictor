c73c809113355458999680c82d9e9ad4fd5d763d
moon chanwoo <159882753+urbanking@users.noreply.github.com>
2024-08-21 02:39:45 +0900

    Update __init__.py

--- app/core/eco_analysis/__init__.py
+++ app/core/eco_analysis/__init__.py
@@ -1,32 +1,24 @@
+from merge import CryptoAndMarketDataCollector
 import schedule
 import time
-import pymongo
-import pandas as pd
-from datetime import datetime
-from fear_class import FearGreedIndexCollector  # fear_class.py 파일에서 FearGreedIndexCollector 클래스 가져오기
 
-# MongoDB 설정
-client = pymongo.MongoClient("mongodb://localhost:27017/")  # MongoDB 서버 연결
-db = client['crypto_database']  # 사용할 데이터베이스 선택
-collection = db['fear_greed_index']  # 사용할 컬렉션 선택
+def update_and_save(collector):
+    collector.update_data()  # Update the data
+    collector.save_to_csv()  # Save the data to CSV
+    collector.save_to_mongo()  # Save the new data to MongoDB
 
-# 1분마다 Fear and Greed Index 데이터를 수집하고 MongoDB에 저장하는 함수
-def fetch_and_store_fear_greed_index():
-    collector = FearGreedIndexCollector()
-    df_minutely = collector.get_fear_greed_index()
+if __name__ == "__main__":
+    start_date = input("Enter the start date (YYYY-MM-DD): ")
 
-    # MongoDB에 데이터 저장
-    records = df_minutely.reset_index().to_dict('records')
-    if records:
-        collection.insert_many(records)
-        print(f"{datetime.now()} - Fear and Greed Index data stored in MongoDB")
-    else:
-        print(f"{datetime.now()} - No data to store")
+    collector = CryptoAndMarketDataCollector(crypto_symbol="BTCUSDT", start_date=start_date, fred_api_key="your_fred_api_key")
 
-# 스케줄링 설정
-schedule.every(1).minutes.do(fetch_and_store_fear_greed_index)
+    # Update and save the data initially
+    update_and_save(collector)
 
-# 스케줄 실행
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    # Schedule to run the update_and_save function every minute
+    schedule.every(1).minutes.do(update_and_save, collector=collector)
+
+    # Keep the script running and performing the scheduled tasks
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
